8cc4f8c90a1c144e06686f0fb033376934e400ed
Shubham <shubham11729@gmail.com>
2023-05-05 08:33:12 -0500

    changes made

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,7 +3,7 @@
 import { select, print } from './utils.js';
 
 
-
+const display = document.querySelector('.display');
 const url = './assets/script/cities.json';
 const url2 = './assets/script/movies.json';
 
@@ -23,7 +23,6 @@ async function getCities() {
 
     const data = await response.json();
     print(data.cities);
-
     
 }
   catch(error){
@@ -32,103 +31,34 @@ async function getCities() {
 }
 
 async function getMovies() {
-    try{
-        const response = await fetch(url2, options );
-     if (!response.ok){
-     throw new Error(`${response.statusText} (${response.status})`);
+    try {
+      const response = await fetch(url2, options); 
+  
+      if (!response.ok) {
+        throw new Error(`${response.statusText} (${response.status})`);
+      }
+  
+      const data = await response.json(); 
+      showMovies(data.movies);
+    } catch(error) {
+      alert(error.message);
     }
+  }
 
-    const data = await response.json();
-    const grid = select(".grid-container");
-    function get(array) {
-
-        grid.innerHTML = "";
-        
-        
-        
-        
-        array.forEach((element) => {
-        
-            grid.innerHTML += ` <div class="grid-item">
-        
-               <div class="poster">
-        
-                <img
-        
-                 src="${element.image}"
-        
-                 alt="movie-poster"
-        
-                />
-        
-               </div>
-        
-               <p class="movie-title"><strong>${element.name}</strong><br>(${element.year})</p>
-        
-           `;
-        
-        });
-    
-}get(data.result)
-}
-  catch(error){
-        print(error.message);
+function showMovies(array) {
+    display.innerHTML = '';
+  
+    if (array.length > 0) {
+      array.forEach(image => {
+        display.innerHTML += `
+        <div class="poster">
+          <img class="poster-image" src="${image.image}">
+          <div class="poster-name">${image.name}</div>
+        </div>
+        `;
+      });
     }
-}
-
-
-
-
-// function showMovies() {
-//     fetch(url2)
-//         .then(response => response.json())
-//         .then(data => {
-//            if (data && data.length >= 10) {
-//                document.querySelector('.lower').innerHTML += `
-//                <div class='box'>
-//                     <img class="img" src=${data[0].image}>
-//                     <h3>${data[0].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[1].image}>
-//                     <h3>${data[1].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[2].image}>
-//                     <h3>${data[2].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[3].image}>
-//                     <h3>${data[3].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[4].image}>
-//                     <h3>${data[4].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[5].image}>
-//                     <h3>${data[5].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[6].image}>
-//                     <h3>${data[6].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[7].image}>
-//                     <h3>${data[7].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[8].image}>
-//                     <h3>${data[8].name}</h3>
-//                 </div>
-//                 <div class='box'>
-//                     <img class="img" src=${data[9].image}>
-//                     <h3>${data[9].name}</h3>
-//                 </div>
-//                `
-//            } 
-//         })
-// }
-
+  }
+showMovies();
 getCities();
 getMovies();
